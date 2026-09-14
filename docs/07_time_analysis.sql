342e8e5435f2564de5106a13cd744f10e7dbb216
RahulSoni0311 <161428354+RahulSoni0311@users.noreply.github.com>
2026-09-14 14:28:23 +0530

    Add time analysis SQL script for sales data
    This SQL script analyzes sales data over time, tracking total sales, customers, and quantity by year and month.

---
+++ docs/07_time_analysis.sql
@@ -0,0 +1,27 @@
+/*
+===============================================================================
+Change Over Time Analysis
+===============================================================================
+Purpose:
+    - To track trends, growth, and changes in key metrics over time.
+    - For time-series analysis and identifying seasonality.
+    - To measure growth or decline over specific periods.
+
+SQL Functions Used:
+    - Date Functions: DATEPART(), DATETRUNC(), FORMAT()
+    - Aggregate Functions: SUM(), COUNT(), AVG()
+===============================================================================
+*/
+
+SELECT
+	DATE_FORMAT(order_date, '%Y') AS order_date,
+    MONTH(order_date) AS order_date,
+    SUM(sales_amount) AS total_sales,
+    COUNT(DISTINCT customer_key) AS total_customers,
+    SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY DATE_FORMAT(order_date, '%Y'), MONTH(order_date)
+ORDER BY DATE_FORMAT(order_date, '%Y'), MONTH(order_date);
+
+
